3b2081c0197720be43bf2a12b32a95ca96511b8e
JinhuanLei <lei,jinhuan@uwlax.edu>
2018-10-05 19:20:42 -0500

    solve os inputs

--- KDTree.py
+++ KDTree.py
@@ -1,13 +1,36 @@
 import sys
 import os
-module_path = os.path.dirname(__file__)
-def GetInputs():
-    fileName=sys.argv[1]
-    minSize=sys.argv[2]
-    print(fileName+" "+minSize)
-    # print(os.getcwd())
-    # print(module_path)
+def getInputs():
+    arguments = sys.argv
+    if len(arguments) != 3:
+        print("Please Enter Correct Inputs !")
+        return
+    fileName = sys.argv[1]
+    minSize = sys.argv[2]
+    # print(os.path.exists(module_path+"2d_large.txt"))
+    # print(module_path+"/"+fileName)
+    module_path = os.path.dirname(__file__)
+    dataFile = module_path + fileName
+    if not os.path.exists(dataFile):
+        print("Please Enter Correct File name !")
+        return
+    inputData = []
+    with open(dataFile, "r") as f:
+        # data=f.readlines()
+        for line in f.readlines():
+            line = line.strip()
+            inputData.append(list(map(str, line.split(" "))))
+    dimentionType = inputData[0]
+    dataSet = inputData[1:]
+    print(dimentionType)
+    # print(dataSet)
+    BuildTree(dataSet,dimentionType,minSize)
 
 
-if __name__=="__main__":
-	GetInputs()
+def BuildTree(dataSet,dimentionType,minSize):
+    pass
+
+
+
+if __name__ == "__main__":
+    getInputs()
