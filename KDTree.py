a3394ec46b3d7be45d95270b8c2df2077107df09
leijinhuan <lei.jinhuan@uwlax.edu>
2018-10-09 20:32:32 -0500

    fixed show path and IO sentences

--- KDTree.py
+++ KDTree.py
@@ -4,11 +4,13 @@ import queue
 
 minSize = 0
 dimensionType = 0
+leaves = {}
+module_path = os.path.dirname(__file__)
 
 
 def getInputs():
     global minSize, dimensionType
-    sys.argv.append("/2d_small.txt")  # necessary command for debug
+    sys.argv.append("2d_small.txt")  # necessary command for debug
     sys.argv.append("2")
     arguments = sys.argv
     if len(arguments) != 3:
@@ -18,23 +20,45 @@ def getInputs():
     minSize = int(sys.argv[2])
     # print(os.path.exists(module_path+"2d_large.txt"))
     # print(module_path+"/"+fileName)
-    module_path = os.path.dirname(__file__)
     dataFile = module_path + "/" + fileName  # pycharm can ignore this / , but in terminal cannot
     if not os.path.exists(dataFile):
         print("Please Enter Correct File name !")
         return
     inputData = []
     with open(dataFile, "r") as f:
-        # data=f.readlines()
         for line in f.readlines():
             line = line.strip()
             inputData.append(list(map(float, line.split(" "))))
     dimensionType = int(inputData[0][0])
     dataSet = inputData[1:]
-    # print(dimensionType)
     root = BuildTree(dataSet, 0)
-    # print(root)
-    traversalTreeInPreOrder(root)
+    traversalTreeInPreOrder(root, "")
+    printTreeLeavesIO()
+
+
+def printTreeLeavesIO():
+    choose = str(input("Print tree leaves? (Enter Y for yes, anything else for no): "))
+    if choose == "Y" or choose == "y":
+        count = 0
+        for key, value in leaves.items():
+            print(str(count) + ".", key, ":", "Bounding Box: ", getBoundingValue(value))
+            print("Data in Leaf: ", value)
+            count += 1
+
+    temp = str(input("Test data? (Enter Y for yes, anything else to quit): "))
+    if temp == "Y" or temp == "y":
+        while True:
+            inputTestFileName = str(input("Name of data-file: "))
+            testFileName = module_path + "/" + inputTestFileName
+            if not os.path.exists(testFileName):
+                print("Please Enter Correct File name !")
+                continue
+            else:
+                print("Correct File")
+    else:
+        print("________________________________")
+        print("Quit.")
+        return
 
 
 def showNode(root):
@@ -42,28 +66,42 @@ def showNode(root):
     print()
 
 
-def traversalTreeInPreOrder(root):  # preOrder
-    if root == None:
-        return
-    showNode(root)
-    traversalTreeInPreOrder(root.left)
-    traversalTreeInPreOrder(root.right)
+def showLeaf(root, path):
+    print(path, " : ", root.__dict__["nodes"])
+    print()
 
 
+def traversalTreeInPreOrder(root, path):
+    results = []
+    return traversalTreeInPreOrderHelper(root, path, results)
 
-def traversalTreeInBFS(root):
-    pass
+
+def traversalTreeInPreOrderHelper(root, path, results):  # preOrder recursive
+    if root is None:
+        return results
+    traversalTreeInPreOrderHelper(root.left, path + "L", results)
+    traversalTreeInPreOrderHelper(root.right, path + "R", results)
+    if (root.left is None) & (root.right is None):
+        results.append(path)
+        # showLeaf(root, path)
+        leaves[path] = root.__dict__["nodes"]
+    return results
 
 
-def showLeafValue(root):
+def getBoundingValue(dataSets):
     # calculate Bounding value
-    dataSets = root.nodes
+    # dataSets = root.nodes
+    # print(dataSets)
     minBounding = []
     maxBounding = []
     for d in range(dimensionType):
         minBounding.append(findMin(dataSets, d))
         maxBounding.append(findMax(dataSets, d))
-    print("Bounding Box : ", minBounding + " , ", maxBounding)
+    # print("Bounding Box : ", minBounding , " , ", maxBounding)
+    answer = []
+    answer.append(minBounding)
+    answer.append(maxBounding)
+    return answer
 
 
 def findMin(dataSet, dimension):
